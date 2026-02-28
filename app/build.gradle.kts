c491e494d15438e31140da8e8df9e5502e997434
Rodrigo Cericatto <graffiti75@gmail.com>
2026-02-28 17:20:54 -0300

    Fix Gradle script to generate APK files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,11 +34,21 @@ android {
 		sourceCompatibility = JavaVersion.VERSION_17
 		targetCompatibility = JavaVersion.VERSION_17
 	}
-	kotlinOptions {
-		jvmTarget = "17"
-	}
 	buildFeatures {
 		compose = true
+	}
+	packaging {
+		resources {
+			excludes += "META-INF/LICENSE.md"
+			excludes += "META-INF/LICENSE-notice.md"
+			excludes += "META-INF/NOTICE.md"
+		}
+	}
+}
+
+kotlin {
+	compilerOptions {
+		jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
 	}
 }
 
